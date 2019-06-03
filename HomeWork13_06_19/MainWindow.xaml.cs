9c3d045b72ac4795d1a9c6ebc2f70361116ac13b
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-06-03 21:02:58 +0600

    Home Work 13.06.19

---
+++ HomeWork13_06_19/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+
+namespace HomeWork13_06_19
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DownloadButtonClick(object sender, RoutedEventArgs e)
+        {
+            var downloadFileDomain = AppDomain.CreateDomain("Download File");
+
+            downloadFileDomain.ExecuteAssembly(downloadFileDomain.BaseDirectory + "DownloadFile.exe");
+            MessageBox.Show("Файл Скачен!\n" +
+                            $"Его расопложение: {downloadFileDomain.BaseDirectory}\n" +
+                            "Имя файла: TestFile.zip");
+            AppDomain.Unload(downloadFileDomain);
+
+        }
+
+        private void FactorialCountingButtonClick(object sender, RoutedEventArgs e)
+        {
+            var factorialCountingDomain = AppDomain.CreateDomain("Factorial Count");
+
+            factorialCountingDomain.ExecuteAssembly(factorialCountingDomain.BaseDirectory + "Factorial.exe");
+            MessageBox.Show("Рандомный факториал найден!\n" +
+                            $"Его расопложение: {factorialCountingDomain.BaseDirectory}\n" +
+                            "Имя файла: FactorialResult.txt");
+            AppDomain.Unload(factorialCountingDomain);
+        }
+    }
+}
